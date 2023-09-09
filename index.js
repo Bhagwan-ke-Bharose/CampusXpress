c28248609221871cc942d249f6b90ddba689538b
Aditya Chandra <aditya.anil.chandra@gmail.com>
2023-09-10 01:19:54 +0530

    10.09.2023

---
+++ index.js
@@ -0,0 +1,227 @@
+function change1() {
+    // let img = document.createElement('img');
+
+    let img = document.getElementById('image');
+    img.src = './logo/ducati.webp';
+    img.style.height = "300px";
+    img.style.border = "1px solid #dee2e6";
+    let detail1 = document.getElementById('detail1');
+    let detail2 = document.getElementById('detail2');
+    let detail3 = document.getElementById('detail3');
+    let detail4 = document.getElementById('detail4');
+    detail1.innerHTML = "Vehicle Name: Ducati";
+    detail1.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail1.style.fontSize = "15px";
+    detail1.style.fontWeight = "500";
+    detail2.innerHTML = "Vehicle Number: 0001";
+    detail2.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail2.style.fontWeight = "500";
+    detail2.style.fontSize = "15px";
+    detail3.innerHTML = "Retailer Name: Kausik";
+    detail3.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail3.style.fontWeight = "500";
+    detail3.style.fontSize = "15px";
+    detail4.innerHTML = "Retailer Number: 0000000001";
+    detail4.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontWeight = "500";
+    detail4.style.borderBottom = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontSize = "15px";
+    // document.getElementsByClassName('ser2').appendChild(img);
+    // element.classList.add("img-thumbnail");
+}
+function change2() {
+    let img = document.getElementById('image');
+    img.src = './logo/ktm.webp';
+    img.style.height = "300px";
+    img.style.border = "1px solid #dee2e6";
+    let detail1 = document.getElementById('detail1');
+    let detail2 = document.getElementById('detail2');
+    let detail3 = document.getElementById('detail3');
+    let detail4 = document.getElementById('detail4');
+    detail1.innerHTML = "Vehicle Name: Ducati";
+    detail1.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail1.style.fontSize = "15px";
+    detail1.style.fontWeight = "500";
+    detail2.innerHTML = "Vehicle Number: 0001";
+    detail2.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail2.style.fontWeight = "500";
+    detail2.style.fontSize = "15px";
+    detail3.innerHTML = "Retailer Name: Kausik";
+    detail3.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail3.style.fontWeight = "500";
+    detail3.style.fontSize = "15px";
+    detail4.innerHTML = "Retailer Number: 0000000001";
+    detail4.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontWeight = "500";
+    detail4.style.borderBottom = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontSize = "15px";
+}
+function change3() {
+    let img = document.getElementById('image');
+    img.src = './logo/bmw.jpg';
+    img.style.height = "300px";
+    img.style.border = "1px solid #dee2e6";
+    let detail1 = document.getElementById('detail1');
+    let detail2 = document.getElementById('detail2');
+    let detail3 = document.getElementById('detail3');
+    let detail4 = document.getElementById('detail4');
+    detail1.innerHTML = "Vehicle Name: Ducati";
+    detail1.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail1.style.fontSize = "15px";
+    detail1.style.fontWeight = "500";
+    detail2.innerHTML = "Vehicle Number: 0001";
+    detail2.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail2.style.fontWeight = "500";
+    detail2.style.fontSize = "15px";
+    detail3.innerHTML = "Retailer Name: Kausik";
+    detail3.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail3.style.fontWeight = "500";
+    detail3.style.fontSize = "15px";
+    detail4.innerHTML = "Retailer Number: 0000000001";
+    detail4.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontWeight = "500";
+    detail4.style.borderBottom = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontSize = "15px";
+}
+function change4() {
+    let img = document.getElementById('image');
+    img.src = './logo/80-mercedes-amg-gt63-e-performance-hero.jpg';
+    img.style.height = "300px";
+    img.style.border = "1px solid #dee2e6";
+    let detail1 = document.getElementById('detail1');
+    let detail2 = document.getElementById('detail2');
+    let detail3 = document.getElementById('detail3');
+    let detail4 = document.getElementById('detail4');
+    detail1.innerHTML = "Vehicle Name: Ducati";
+    detail1.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail1.style.fontSize = "15px";
+    detail1.style.fontWeight = "500";
+    detail2.innerHTML = "Vehicle Number: 0001";
+    detail2.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail2.style.fontWeight = "500";
+    detail2.style.fontSize = "15px";
+    detail3.innerHTML = "Retailer Name: Kausik";
+    detail3.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail3.style.fontWeight = "500";
+    detail3.style.fontSize = "15px";
+    detail4.innerHTML = "Retailer Number: 0000000001";
+    detail4.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontWeight = "500";
+    detail4.style.borderBottom = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontSize = "15px";
+}
+function change5() {
+    let img = document.getElementById('image');
+    img.src = './logo/eugene-tkachenko-poWsLrpntc8-unsplash2.jpg';
+    img.style.height = "300px";
+    img.style.border = "1px solid #dee2e6";
+    let detail1 = document.getElementById('detail1');
+    let detail2 = document.getElementById('detail2');
+    let detail3 = document.getElementById('detail3');
+    let detail4 = document.getElementById('detail4');
+    detail1.innerHTML = "Vehicle Name: Ducati";
+    detail1.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail1.style.fontSize = "15px";
+    detail1.style.fontWeight = "500";
+    detail2.innerHTML = "Vehicle Number: 0001";
+    detail2.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail2.style.fontWeight = "500";
+    detail2.style.fontSize = "15px";
+    detail3.innerHTML = "Retailer Name: Kausik";
+    detail3.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail3.style.fontWeight = "500";
+    detail3.style.fontSize = "15px";
+    detail4.innerHTML = "Retailer Number: 0000000001";
+    detail4.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontWeight = "500";
+    detail4.style.borderBottom = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontSize = "15px";
+}
+function change6() {
+    let img = document.getElementById('image');
+    img.src = './logo/2007-Bugatti-Veyron-review-classic-MOTOR.jpg';
+    img.style.height = "300px";
+    img.style.border = "1px solid #dee2e6";
+    let detail1 = document.getElementById('detail1');
+    let detail2 = document.getElementById('detail2');
+    let detail3 = document.getElementById('detail3');
+    let detail4 = document.getElementById('detail4');
+    detail1.innerHTML = "Vehicle Name: Ducati";
+    detail1.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail1.style.fontSize = "15px";
+    detail1.style.fontWeight = "500";
+    detail2.innerHTML = "Vehicle Number: 0001";
+    detail2.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail2.style.fontWeight = "500";
+    detail2.style.fontSize = "15px";
+    detail3.innerHTML = "Retailer Name: Kausik";
+    detail3.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail3.style.fontWeight = "500";
+    detail3.style.fontSize = "15px";
+    detail4.innerHTML = "Retailer Number: 0000000001";
+    detail4.style.border = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontWeight = "500";
+    detail4.style.borderBottom = "1px solid rgba(0,0,0, .125)"; 
+    detail4.style.fontSize = "15px";
+}
+let subtotal1 = 0;
+let subtotal2 = 0;
+let subtotal3 = 0;
+let subtotal4 = 0;
+let subtotal5 = 0;
+let subtotal6 = 0;
+function fun1() {
+    let subtotal = document.getElementById('sub1');
+    let val = document.getElementById('val1').value;
+    subtotal.innerHTML = `$${val * 1000}`;
+    subtotal1 = val*1000;
+    // sum = sum + val * 1000;
+    let total = document.getElementById('total');
+    total.innerHTML = `Total: $${subtotal1 + subtotal2 + subtotal3 + subtotal4 + subtotal5 + subtotal6}`;
+}
+function fun2() {
+    let subtotal = document.getElementById('sub2');
+    let val = document.getElementById('val2').value;
+    subtotal.innerHTML = `$${val * 1000}`;
+    // let subtotal2 = subtotal.innerHTML;
+    subtotal2 = val * 1000;
+    // sum = sum + val * 1000;
+    let total = document.getElementById('total');
+    total.innerHTML = `Total: $${subtotal1 + subtotal2 + subtotal3 + subtotal4 + subtotal5 + subtotal6}`;
+}
+function fun3() {
+    let subtotal = document.getElementById('sub3');
+    let val = document.getElementById('val3').value;
+    subtotal.innerHTML = `$${val * 1000}`;
+    subtotal3 = val * 1000;
+    // sum = sum + val * 1000;
+    let total = document.getElementById('total');
+    total.innerHTML = `Total: $${subtotal1 + subtotal2 + subtotal3 + subtotal4 + subtotal5 + subtotal6}`;
+}
+function fun4() {
+    let subtotal = document.getElementById('sub4');
+    let val = document.getElementById('val4').value;
+    subtotal.innerHTML = `$${val * 1000}`;
+    subtotal4 = val * 1000;
+    // sum = sum + val * 1000;
+    let total = document.getElementById('total');
+    total.innerHTML = `Total: $${subtotal1 + subtotal2 + subtotal3 + subtotal4 + subtotal5 + subtotal6}`;
+}
+function fun5() {
+    let subtotal = document.getElementById('sub5');
+    let val = document.getElementById('val5').value;
+    subtotal.innerHTML = `$${val * 1000}`;
+    subtotal5 = val * 1000;
+    // sum = sum + val * 1000;
+    let total = document.getElementById('total');
+    total.innerHTML = `Total: $${subtotal1 + subtotal2 + subtotal3 + subtotal4 + subtotal5 + subtotal6}`;
+}
+function fun6() {
+    let subtotal = document.getElementById('sub6');
+    let val = document.getElementById('val6').value;
+    subtotal.innerHTML = `$${val * 1000}`;
+    subtotal6 = val * 1000;
+    // sum = sum + val * 1000;
+    let total = document.getElementById('total');
+    total.innerHTML = `Total: $${subtotal1 + subtotal2 + subtotal3 + subtotal4 + subtotal5 + subtotal6}`;
+}
